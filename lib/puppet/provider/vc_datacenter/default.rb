a9334f5bc8a1547e5f4d771317dd8697fbbcc1db
HarryPan <harry@puppetlabs.com>
2012-05-02 10:21:05 -0700

    Add comments on create.

--- lib/puppet/provider/vc_datacenter/default.rb
+++ lib/puppet/provider/vc_datacenter/default.rb
@@ -11,7 +11,7 @@ Puppet::Type.type(:vc_datacenter).provide(:vc_datacenter) do
                                    user: user,
                                    password: pwd,
                                    insecure: true
-    @rootFolder = @conn.serviceInstance.content.rootFolder
+    @rootFolder ||= @conn.serviceInstance.content.rootFolder
 
     # TODO for now path must be in the form of '/foo/bar/dc/'
     @parent_path ||= @resource[:path][1..@resource[:path].size-2].split('/')
@@ -27,10 +27,11 @@ Puppet::Type.type(:vc_datacenter).provide(:vc_datacenter) do
       # TODO ASSUMPTION each level is either a Folder (has a find method)
       # or a Datacenter (.hostFolder has a find method)
 
-      # under the above assumption, if current_lv is doesn't have a find
+      # Under the above assumption, if current_lv is doesn't have a find
       # method, we actually want its hostFolder
       current_lv = current_lv.hostFolder if !current_lv.class.method_defined? 'find'
-      # go one level deeper
+
+      # Go one level deeper.  Raise an error if we can't.
       current_lv = current_lv.find lv
       raise Puppet::Error.new('Invalid path for datacenter' + @resource[:path]) if !current_lv
     end
@@ -43,22 +44,27 @@ Puppet::Type.type(:vc_datacenter).provide(:vc_datacenter) do
   end
 
   def create
-    # create vCenter datacenter resource[:path]
+    # TODO If there's a datacenter at any level in the path (not only
+    # the immediate-parent level), this is not going to work due to the
+    # limitation of vcenter itself.  We can let vcenter report it or we
+    # can report it ourselves.  Currently we leave it to vcenter.  The
+    # bad thing about it is ensuring /folder/dc/folder/dc/ present is
+    # gonna fail but ensuring its absence will succeed.
     setup
     @immediate_parent.CreateDatacenter name: @dcname
   end
 
   def destroy
-    # delete vCenter datacenter resource[:path]
     setup
-    # TODO make sure @dcname is a Datacenter!
-    @immediate_parent.find(@dcname).Destroy_Task.wait_for_completion
+    dc = @immediate_parent.find @dcname
+    dc.Destroy_Task.wait_for_completion if dc.is_a? RbVmomi::VIM::Datacenter
   end
 
   def exists?
     # verify if datacenter exists
     setup
-    # TODO make sure @dcname is a Datacenter!
-    return @immediate_parent.find @dcname
+    dc = @immediate_parent.find @dcname
+    return dc.is_a? RbVmomi::VIM::Datacenter
   end
 end
+
