05924494cf24c8eb1c2e71933d34b48a49692ad2
HarryPan <harry@puppetlabs.com>
2012-05-04 09:47:33 -0700

    Small improvement in module declaration.

--- lib/puppet/modules/vcenter.rb
+++ lib/puppet/modules/vcenter.rb
@@ -1,34 +1,31 @@
 require 'rbvmomi'
 
-module Puppet
-  module Modules
-    module VCenter
-      def find_immediate_parent root_folder, parent_lvs, error_msg
-        current_lv = root_folder
-        parent_lvs.each do |lv|
-          # TODO ASSUMPTION each level is either a Folder (has a find method)
-          # or a Datacenter (.hostFolder has a find method)
+module Puppet::Modules
+  module VCenter
+    def find_immediate_parent root_folder, parent_lvs, error_msg
+      current_lv = root_folder
+      parent_lvs.each do |lv|
+        # TODO ASSUMPTION each level is either a Folder (has a find method)
+        # or a Datacenter (.hostFolder has a find method)
 
-          # Under the above assumption, if current_lv is doesn't have a find
-          # method, we actually want its hostFolder
-          unless current_lv.class.method_defined? 'find'
-            current_lv = current_lv.hostFolder
-          end
-
-          # Go one level deeper.  Raise an error if we can't.
-          current_lv = current_lv.find lv
-          unless current_lv
-            raise Puppet::Error.new error_msg
-          end
+        # Under the above assumption, if current_lv is doesn't have a find
+        # method, we actually want its hostFolder
+        unless current_lv.class.method_defined? 'find'
+          current_lv = current_lv.hostFolder
         end
 
-        if current_lv.is_a? RbVmomi::VIM::Datacenter
-          current_lv.hostFolder 
-        else
-          current_lv
+        # Go one level deeper.  Raise an error if we can't.
+        current_lv = current_lv.find lv
+        unless current_lv
+          raise Puppet::Error.new error_msg
         end
+      end
+
+      if current_lv.is_a? RbVmomi::VIM::Datacenter
+        current_lv.hostFolder 
+      else
+        current_lv
       end
     end
   end
 end
-
