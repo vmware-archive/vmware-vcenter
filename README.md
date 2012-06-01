6ce03d328c1da507992f57be14431943ede05ba0
Nan Liu <nan@puppetlabs.com>
2012-06-01 13:34:16 +0000

    Misc updates to module.
    * Increase installation timeout.
    * Refactor registry resources to use resource defaults.
    * Update README for typos and parameter changes.

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # vcenter puppet module
 
-VMware vCenter management
+VMware vCenter installation and management
 
-## Install
+## Installation
 
-Paramaters:
+Parameters:
 
-* media: installation softare media location.
+* media: vCenter installation software media location.
+* sql_media: Microsoft SQL installation software media location.
 * username: vcenter service account (default: VCENTER).
-* password: vcenter service account password (default: 'vCenter2008demo').
+* password: vcenter service account password (default: 'vC3nt!2008demo'),
 * jvm_memory_option: vcenter sql server size, support S, M, L.
 * client: install vsphere client software (default: true).
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class vcenter (
   $media             = 'D:\\',
   $sql_media         = 'D:\\',
   $username          = 'VCENTER',
-  $password          = 'vCenter2008demo',
+  $password          = 'vC3nt!2008demo',
   $jvm_memory_option = 'S',
   $client            = true
 ) {
@@ -28,63 +28,64 @@ class vcenter (
   $sqlcmd = '"C:\Program Files\Microsoft SQL Server\100\Tools\Binn\sqlcmd.exe"'
 
   exec { 'create_database':
-    command => "${sqlcmd} -Q \"if not exists(select * from sys.databases where name='vcenter') create database [vcenter]; alter database [vcenter] set recovery simple;\"",
-    path    => $::path,
-    require => Class['mssql'],
+    command     => "${sqlcmd} -Q \"if not exists(select * from sys.databases where name='vcenter') create database [vcenter]; alter database [vcenter] set recovery simple;\"",
+    path        => $::path,
+    refreshonly => true,
+    subscribe   => Class['mssql'],
   }
 
   registry_key { [ 'HKLM\SOFTWARE\ODBC',
-                   'HKLM\SOFTWARE\ODBC\ODBC.INI',
-                   'HKLM\SOFTWARE\ODBC\ODBC.INI\ODBC Data Sources',
-                   'HKLM\SOFTWARE\ODBC\ODBC.INI\VMware VirtualCenter' ]:
+                   'HKLM\SOFTWARE\ODBC\ODBC.INI' ]:
     ensure => present,
   }
 
   Registry::Value {
+    key   => 'HKLM\SOFTWARE\ODBC\ODBC.INI\VMware VirtualCenter',
     notify => Exec['create_database'],
     before => Exec['install_vCenter'],
   }
 
   registry::value { 'VMware VirtualCenter':
     key   => 'HKLM\SOFTWARE\ODBC\ODBC.INI\ODBC Data Sources',
-    value => 'SQL Server Native Client 10.0',
+    value => 'VMware VirtualCenter',
+    data  => 'SQL Server Native Client 10.0',
     type  => string,
   }
 
   registry::value { 'Driver':
-    key   => 'HKLM\SOFTWARE\ODBC\ODBC.INI\VMware VirtualCenter',
-    value => "C:\\Windows\\system32\\sqlncli10.dll",
+    value => 'Driver',
+    data  => "C:\\Windows\\system32\\sqlncli10.dll",
     type  => string,
   }
 
   registry::value { 'Server':
-    key   => 'HKLM\SOFTWARE\ODBC\ODBC.INI\VMware VirtualCenter',
-    value => '(local)',
+    value => 'Server',
+    data  => '(local)',
     type  => string,
   }
 
   registry::value { 'Database':
-    key   => 'HKLM\SOFTWARE\ODBC\ODBC.INI\VMware VirtualCenter',
-    value => 'vcenter',
+    value => 'Database',
+    data  => 'vcenter',
     type  => string,
   }
 
   registry::value { 'LastUser':
-    key   => 'HKLM\SOFTWARE\ODBC\ODBC.INI\VMware VirtualCenter',
-    value => 'Administrator',
+    value => 'LastUser',
+    data  => 'Administrator',
     type  => string,
   }
 
   registry::value { 'Trusted_Connection':
-    key   => 'HKLM\SOFTWARE\ODBC\ODBC.INI\VMware VirtualCenter',
-    value => 'Yes',
+    value => 'Trusted_Connection',
+    data  => 'Yes',
     type  => string,
   }
 
   exec { 'install_vCenter':
     command => 'vCenter-Server\\VMware-vcserver.exe /s /w /L1033 /v"/qr USERNAME=Administrator COMPANYNAME=Puppet DB_SERVER_TYPE=Custom DB_DSN=\"VMWARE VirtualCenter\" DB_DSN_WINDOWS_AUTH=1 FORMAT_DB=1"',
     path    => $media,
-    timeout => 900,
+    timeout => 1200,
     require => Class['mssql'],
   }
 
@@ -92,7 +93,7 @@ class vcenter (
     exec { 'install_vSphere_client':
       command => 'vSphere-Client\\VMware-viclient.exe /s /w /L1033 /v" /qr"',
       path    => $media,
-      timeout => 300,
+      timeout => 600,
       require => Exec['install_vCenter'],
     }
   }
