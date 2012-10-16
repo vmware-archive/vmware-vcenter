0863df744f718c456de3f4cc59f3f5bb5e10cdd3
Nan Liu <nan.liu@gmail.com>
2012-10-16 13:20:33 -0700

    Fix accept to alias present.

--- lib/puppet/type/vcsa_eula.rb
+++ lib/puppet/type/vcsa_eula.rb
@@ -2,16 +2,11 @@ Puppet::Type.newtype(:vcsa_eula) do
   @doc = 'Manage vCSA EULA.'
 
   ensurable do
-    newvalue(:accept) do
+    newvalue(:present) do
       provider.accept
     end
 
-#    newvalue(:absent) do
-#      provider.destroy
-#    end
-#
-#    aliasvalue(:true, :accept)
-#    aliasvalue(:false, :absent)
+    aliasvalue(:accept, :present)
   end
 
   newparam(:name, :namevar => true) do
