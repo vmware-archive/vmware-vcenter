803982bd2dbb1d12740f06da3ebe6031b77b9f24
Nan Liu <nan.liu@gmail.com>
2012-11-19 17:37:22 -0800

    Change transport retrieval to support multiple connection.
    Originally it was intended for one transport per resource, but for
    vShield we need multiple transport per resource and the ability to
    discover the vShield associated vCenter system without the user
    specifying the connection. This update allows discovery of the
    associated vCenter system through vShield API calls, but use the
    connection info from the manifest (since it's not available in vShield).

--- lib/puppet/provider/vcsa.rb
+++ lib/puppet/provider/vcsa.rb
@@ -4,7 +4,7 @@ require 'lib/puppet_x/puppetlabs/transport/ssh'
 class Puppet::Provider::Vcsa <  Puppet::Provider
 
   def self.transport(resource)
-    @transport ||= PuppetX::Puppetlabs::Transport.retrieve(resource[:transport], resource.catalog, 'ssh')
+    @transport ||= PuppetX::Puppetlabs::Transport.retrieve(:resource_ref => resource[:transport], :catalog => resource.catalog, :provider => 'ssh')
   end
 
   def transport
