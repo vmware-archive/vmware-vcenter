a08d58f71abc508159c14994323ab6e858788b55
sholland@vmware.com <sholland@vmware.com>
2013-01-07 14:45:45 -0800

    moved a few functions from vshield_ipset to vshield.rb

--- lib/puppet/provider/vshield_ipset/default.rb
+++ lib/puppet/provider/vshield_ipset/default.rb
@@ -3,25 +3,19 @@ require 'puppet/provider/vshield'
 Puppet::Type.type(:vshield_ipset).provide(:default, :parent => Puppet::Provider::Vshield) do
   @doc = 'Manages vShield ipset.'
 
-  def ret_datacenter_id(name=resource[:datacenter_name])
-    dc = vim.serviceInstance.find_datacenter(name) or raise Puppet::Error, "datacenter '#{name}' not found."
-    dc._ref
-  end
-
   def exists?
     begin
-      all_ipsets = ipset_detail || []
+      all_ipsets = ipset_detail || return false
       all_ipsets.each do |list|
         #Puppet.debug("list = #{list['list']['ipset'].inspect}")
         #Puppet.debug("class = #{list['list']['ipset'].class}")
-        @ipset   = list['list']['ipset'].find{|x| x['name'] == resource[:name]} 
+        @ipset   = list['list']['ipset'].find{|x| x['name'] == resource[:name]}
         Puppet.debug("@ipset = #{@ipset.inspect}")
         break
       end
     rescue Exception
     end 
     Puppet.debug("@ipset = #{@ipset.inspect}")
-    #raise Puppet::Error, "end exists"
     @ipset
   end
 
@@ -29,8 +23,8 @@ Puppet::Type.type(:vshield_ipset).provide(:default, :parent => Puppet::Provider:
     Puppet.debug("@ipset = #{@ipset.inspect}")
     Puppet.debug("create ipset #{resource[:name]}")
     data = {}
-    data[:name]     = "#{resource[:name]}"
-    data[:value]    = "#{resource[:value]}" 
+    data[:name]     = resource[:name]
+    data[:value]    = resource[:value]
     data[:revision] = '0'
     Puppet.debug("data = #{data.inspect}")
     post("api/2.0/services/ipset/#{@scope_moref}", { :ipset => data } )
@@ -60,32 +54,4 @@ Puppet::Type.type(:vshield_ipset).provide(:default, :parent => Puppet::Provider:
     put("api/2.0/services/ipset/#{@ipset['objectId']}", data )
   end
 
-  def edge_summary
-    # TODO: This may exceed 256 pagesize limit.
-    @edge_summary ||= [get('api/3.0/edges')['pagedEdgeList']['edgePage']['edgeSummary']].flatten
-  end
-
-  def get_all_ipsets(url)
-    all_ipsets ||= [get(url)].flatten
-  end
-
-  def edge_detail
-    raise Puppet::Error, "edge not available" unless @instance
-    Puppet.debug "@instance = #{@instance}"
-    @edge_detail ||= get("api/3.0/edges/#{@instance['id']}")['edge']
-  end  
-
-  def ipset_detail
-    @scope_moref = ''
-    if resource[:scope_type].to_s == 'datacenter'
-      @scope_moref = ret_datacenter_id(resource[:scope_name])
-      Puppet.debug("datacenter_id = #{datacenter_id.inspect}")
-    else
-      result = edge_summary || []
-      instance = result.find{|x| x['name'] == resource[:scope_name]}
-      #Puppet.debug("instance = #{instance['id'].inspect}")
-      @scope_moref = instance['id']
-    end
-    get_all_ipsets("/api/2.0/services/ipset/scope/#{@scope_moref}")
-  end
 end
