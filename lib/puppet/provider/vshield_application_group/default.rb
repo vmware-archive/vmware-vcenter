eeb0d5aef73d0846b26ca44b780672eae304c785
sholland@vmware.com <sholland@vmware.com>
2013-01-17 10:23:30 -0800

    changed .flatten to use ensure_array and cleaned up vshield_application_group with populate_member

--- lib/puppet/provider/vshield_application_group/default.rb
+++ lib/puppet/provider/vshield_application_group/default.rb
@@ -4,9 +4,20 @@ Puppet::Type.type(:vshield_application_group).provide(:default, :parent => Puppe
   @doc = 'Manages vShield Application Group.'
 
   def exists?
-    results = nested_value(get("/api/2.0/services/applicationgroup/scope/#{vshield_scope_moref}"), ['list', 'applicationGroup'])
+    results = ensure_array( nested_value(get("/api/2.0/services/applicationgroup/scope/#{vshield_scope_moref}"), ['list', 'applicationGroup']) )
     # If there's a single application the result is a hash, while multiple results in an array.
-    @application_group = [results].flatten.find {|application_group| application_group['name'] == resource[:name]} if results
+    @app_group = results.find{|application_group| application_group['name'] == resource[:name]}
+    Puppet.debug("@app_group = #{@app_group.inspect}")
+    populate_member
+    @app_group
+  end
+
+  def populate_member
+    if @app_group
+      @app_group['member'] = ensure_array(@app_group['member'])
+      @app_group['application_member'] = ensure_array(@app_group['application_member'])
+      @app_group['application_group_member'] = ensure_array(@app_group['application_group_member'])
+    end
   end
 
   def create
@@ -15,16 +26,15 @@ Puppet::Type.type(:vshield_application_group).provide(:default, :parent => Puppe
     data = {
       :revision => 0,
       :name     => resource[:name],
-      #:member   => app_id_members
     }
     post("api/2.0/services/applicationgroup/#{vshield_scope_moref}", {:applicationGroup => data} )
 
     @pending_changes = true
-    results = nested_value(get("/api/2.0/services/applicationgroup/scope/#{vshield_scope_moref}"), ['list', 'applicationGroup'])
+    results = ensure_array( nested_value(get("/api/2.0/services/applicationgroup/scope/#{vshield_scope_moref}"), ['list', 'applicationGroup']) )
 
     # If there's a single application the result is a hash, while multiple results in an array.
-    @application_group = [results].flatten.find {|application_group| application_group['name'] == resource[:name]} if results
-
+    @app_group = results.find {|application_group| application_group['name'] == resource[:name]}
+    populate_member
   end
 
   def destroy
@@ -33,14 +43,8 @@ Puppet::Type.type(:vshield_application_group).provide(:default, :parent => Puppe
 
   def application_member
     cur_app_members = []
-    if @application_group and @application_group['member']
-      # horrible, for only 1 member the type returned from vshield is a hash, otherwise array
-      if @application_group['member'].class == Hash
-        @application_group['member'] = [ @application_group['member'] ]
-      end
-      applications    = @application_group['member'].to_a.find_all{ |member| member['objectTypeName'] == 'Application'}
-      cur_app_members = applications.collect{ |member| member['name'] }
-    end
+    applications    = @app_group['member'].to_a.find_all{ |member| member['objectTypeName'] == 'Application'}
+    cur_app_members = applications.collect{ |member| member['name'] }
     cur_app_members.sort
   end
 
@@ -50,14 +54,8 @@ Puppet::Type.type(:vshield_application_group).provide(:default, :parent => Puppe
 
   def application_group_member
     cur_app_members = []
-    if @application_group and @application_group['member']
-      # horrible, for only 1 member the type returned from vshield is a hash, otherwise array
-      if @application_group['member'].class == Hash
-        @application_group['member'] = [ @application_group['member'] ]
-      end
-      applications    = @application_group['member'].to_a.find_all{ |member| member['objectTypeName'] == 'ApplicationGroup'}
-      cur_app_members = applications.collect{ |member| member['name'] }
-    end
+    applications    = @app_group['member'].to_a.find_all{ |member| member['objectTypeName'] == 'ApplicationGroup'}
+    cur_app_members = applications.collect{ |member| member['name'] }
     cur_app_members.sort
   end
 
@@ -66,71 +64,59 @@ Puppet::Type.type(:vshield_application_group).provide(:default, :parent => Puppe
   end
 
   def flush
-    raise Puppet::Error, "Application Group #{resource[:name]} was not found" unless @application_group
     if @pending_changes
-      application_group_id = @application_group['objectId']
+      raise Puppet::Error, "Application Group #{resource[:name]} was not found" unless @app_group
+      application_group_id = @app_group['objectId']
       # for all application_members add ones not currently members
       if resource[:application_member]
         resource[:application_member].each do |app_member|
-          results = nested_value(get("/api/2.0/services/application/scope/#{vshield_scope_moref}"), ['list', 'application'])
-          app     = [results].flatten.find {|application| application['name'] == app_member}
-          # if application does not exist, bomb out
-          if app
-            if @application_group['member']
-              if_member = @application_group['member'].find{ |member| member['name'] == app_member and member['objectTypeName'] == 'Application' }
-            end
+          results = ensure_array( nested_value(get("/api/2.0/services/application/scope/#{vshield_scope_moref}"), ['list', 'application']) )
+          app     = results.find {|application| application['name'] == app_member}
+          raise Puppet::Error, "Application #{app_member} does not exist, it will not be added to #{resource[:name]}" if app.nil?
 
-            if if_member.nil?
-              Puppet.debug("Adding #{app_member} to #{resource[:name]}")
-              put("api/2.0/services/applicationgroup/#{application_group_id}/members/#{app['objectId']}", {} )
-            end
-          else
-            Puppet.warning "Application #{app_member} does not exist, it will not be added to #{resource[:name]}"
+          # if application does not exist, bomb out
+          existing_member = ensure_array( @app_group['member']).find{ |member| member['name'] == app_member and member['objectTypeName'] == 'Application' }
+
+          if existing_member.nil?
+            Puppet.debug("Adding #{app_member} to #{resource[:name]}")
+            put("api/2.0/services/applicationgroup/#{application_group_id}/members/#{app['objectId']}", {} )
           end
         end
-        # for all current application_members remove ones not listed in the resource[:application_members]
-        if @application_group['member']
-          @application_group['member'].each do |app_member|
-            app_name = app_member['name']
-            app_id   = app_member['objectId']
-            unless resource[:application_member].include?(app_name)
-              Puppet.debug("Removing #{app_name} from #{resource[:name]}")
-              delete("api/2.0/services/applicationgroup/#{application_group_id}/members/#{app_id}" )
-            end
+        # for all current application_members remove ones not in resource[:application_members]
+        @app_group['member'] ||= []
+        @app_group['member'].each do |app_member|
+          app_name = app_member['name']
+          app_id   = app_member['objectId']
+          unless resource[:application_member].include?(app_name)
+            Puppet.debug("Removing #{app_name} from #{resource[:name]}")
+            delete("api/2.0/services/applicationgroup/#{application_group_id}/members/#{app_id}" )
           end
-        end 
+        end
       end
 
       # add all application_groups that are not currently members
       if resource[:application_group_member]
         resource[:application_group_member].each do |app_member|
-          results = nested_value(get("/api/2.0/services/applicationgroup/scope/#{vshield_scope_moref}"), ['list', 'applicationGroup'])
-          app     = [results].flatten.find {|app_group| app_group['name'] == app_member and app_group['objectTypeName'] == 'ApplicationGroup' }
-          # if application does not exist, give warning and don't update
-          if app
-            if @application_group['member']
-              if_member = @application_group['member'].find{ |member| member['name'] == app_member }
-            end
+          results = ensure_array( nested_value(get("/api/2.0/services/applicationgroup/scope/#{vshield_scope_moref}"), ['list', 'applicationGroup']) )
+          app     = results.find {|app_group| app_group['name'] == app_member and app_group['objectTypeName'] == 'ApplicationGroup' }
+          # if application does not exist, error out and dont update
+          raise Puppet::Error, "ApplicationGroup #{app_member} does not exist, it will not be added to #{resource[:name]}" if app.nil?
+          existing_member = @app_group['member'].find{ |member| member['name'] == app_member }
 
-            if if_member.nil?
-              Puppet.debug("Adding #{app_member} to #{resource[:name]}")
-              put("api/2.0/services/applicationgroup/#{application_group_id}/members/#{app['objectId']}", {} )
-            end
-          else
-            Puppet.warning "ApplicationGroup #{app_member} does not exist, it will not be added to #{resource[:name]}"
+          if existing_member.nil?
+            Puppet.debug("Adding #{app_member} to #{resource[:name]}")
+            put("api/2.0/services/applicationgroup/#{application_group_id}/members/#{app['objectId']}", {} )
           end
         end
-        # for all current application_members remove ones not listed in the resource[:application_members]
-        if @application_group['member']
-          @application_group['member'].each do |app_member|
-            app_name = app_member['name']
-            app_id   = app_member['objectId']
-            unless resource[:application_group_member].include?(app_name)
-              Puppet.debug("Removing #{app_name} from #{resource[:name]}")
-              delete("api/2.0/services/applicationgroup/#{application_group_id}/members/#{app_id}" )
-            end
+        # for all current application_members remove ones not in resource[:application_members]
+        @app_group['member'].each do |app_member|
+          app_name = app_member['name']
+          app_id   = app_member['objectId']
+          unless resource[:application_group_member].include?(app_name)
+            Puppet.debug("Removing #{app_name} from #{resource[:name]}")
+            delete("api/2.0/services/applicationgroup/#{application_group_id}/members/#{app_id}" )
           end
-        end 
+        end
       end
     end
   end
