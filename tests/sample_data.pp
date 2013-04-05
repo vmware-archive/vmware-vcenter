fb18496a65a37c0994a5ab83c282e2d30693c4d5
sholland@vmware.com <sholland@vmware.com>
2013-04-05 15:44:37 -0700

    added vc_vpxsettings type / provider
    removed autorequire coments

--- tests/sample_data.pp
+++ tests/sample_data.pp
@@ -74,3 +74,12 @@ $vm1 = {
   'name'  => 'test',
   'power_state' => 'poweredOn',
 }
+
+# the complete list is available by: 
+# 1. browsing to https://<vcenter>/mob/?moid=ServiceInstance&method=retrieveContent
+# 2. click on Invoke Method
+# 3. select VpxSettings, just strip the 'setting[]' from any item
+$vpx_settings = {
+  'event.maxAgeEnabled' => false,
+  'event.maxAge'        => l4,
+}
