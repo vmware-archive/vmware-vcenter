905d3f6a383805446c3f744fac1f0b8a92b45996
Nan Liu <nan.liu@gmail.com>
2013-11-15 11:54:32 -0800

    Add spec fixtures.

--- spec/classes/vcenter_package_spec.rb
+++ spec/classes/vcenter_package_spec.rb
@@ -12,12 +12,50 @@ describe 'vcenter::package' do
       should contain_package('net-ssh').
         with_provider('pe_gem')
       should contain_package('hashdiff').with(
-        :version => '0.0.6',
+        :ensure => '0.0.6',
         :provider => 'pe_gem'
       )
-      should contain_package 'nori'
-      should contain_package 'gyoku'
-      should contain_package 'rbvmomi'
+      should contain_package('nori').with(
+        :ensure => '1.1.5',
+        :provider => 'pe_gem'
+      )
+      should contain_package('gyoku').with(
+        :ensure => '1.0.0z2',
+        :provider => 'pe_gem'
+      )
+      should contain_package('rbvmomi').with(
+        :ensure => '1.6.0.z1',
+        :provider => 'pe_gem'
+      )
+    end
+  end
+
+  context 'puppet' do
+    let(:facts){{
+      :puppetversion => '3.2.0',
+    }}
+
+    it do
+      should contain_package('rest-client').
+        with_provider('gem')
+      should contain_package('net-ssh').
+        with_provider('gem')
+      should contain_package('hashdiff').with(
+        :ensure => '0.0.6',
+        :provider => 'gem'
+      )
+      should contain_package('nori').with(
+        :ensure => '1.1.5',
+        :provider => 'gem'
+      )
+      should contain_package('gyoku').with(
+        :ensure => '1.0.0z2',
+        :provider => 'gem'
+      )
+      should contain_package('rbvmomi').with(
+        :ensure => '1.6.0.z1',
+        :provider => 'gem'
+      )
     end
   end
 end
