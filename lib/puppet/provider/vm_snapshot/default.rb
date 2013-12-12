fce2c16a81b1e9c24758f91433df410eb71b1a5d
arpit mittal <arpit_mittal@dell.com>
2013-12-12 09:24:35 +0000

    Enhanced exceptional handling in vm_snapshot/default.rb Reviewed by: AM

--- lib/puppet/provider/vm_snapshot/default.rb
+++ lib/puppet/provider/vm_snapshot/default.rb
@@ -2,11 +2,17 @@
 provider_path = Pathname.new(__FILE__).parent.parent
 require File.join(provider_path, 'vcenter')
 require 'rbvmomi'
+
 Puppet::Type.type(:vm_snapshot).provide(:vm_snapshot, :parent => Puppet::Provider::Vcenter) do
   @doc = "Manage vCenter VMs Snapshot Operation."
   def create
     puts "Inside Create Method."
-    vm.CreateSnapshot_Task(name: resource[:snapshot_name], memory: false, quiesce: true).wait_for_completion
+    begin
+      vm.CreateSnapshot_Task(:name=> resource[:snapshot_name], :memory => false, :quiesce => true).wait_for_completion
+    rescue Exception => e
+      puts "Exception occured with following message:"
+      puts e.message
+    end
   end
 
   def destroy
@@ -15,41 +21,57 @@ Puppet::Type.type(:vm_snapshot).provide(:vm_snapshot, :parent => Puppet::Provide
   def exists?
     puts "Inside the exists method."
     if(resource[:snapshot_operation] == nil)
-    	return false;
+      return false;
     else
-        return true;
+      return true;
     end
   end
 
   def snapshot_operation
   end
- 
-  # Performs the snapshot operation 
+
+  # Performs the snapshot operation
   def snapshot_operation=(value)
-	ss_name = resource[:snapshot_name]
-        snapshot_list = vm.snapshot.rootSnapshotList
-        snapshot = find_node(snapshot_list, ss_name)
-     if value == :revert
-        snapshot.RevertToSnapshot_Task(:suppressPowerOn => false).wait_for_completion 
-      elsif value == :remove
-    	snapshot.RemoveSnapshot_Task(:removeChildren => false).wait_for_completion 
+    begin
+      ss_name = resource[:snapshot_name]
+      vmSnapshot = vm.snapshot
+      if vmSnapshot == nil
+        raise "No snapshots found associated to vm."
       end
+      snapshot_list = vmSnapshot.rootSnapshotList
+      snapshot = find_node(snapshot_list, ss_name)
+      if value == :revert
+        snapshot.RevertToSnapshot_Task(:suppressPowerOn => false).wait_for_completion
+      elsif value == :remove
+        snapshot.RemoveSnapshot_Task(:removeChildren => false).wait_for_completion
+      end
+    rescue Exception => e
+      puts "Exception occured with following message:"
+      puts e.message
+    end
   end
 
   private
-def find_node(tree, name)
-   snapshot = nil
-   tree.each do |node|
-      if node.name == name
-        snapshot = node.snapshot
-      elsif !node.childSnapshotList.empty?
-        snapshot = find_node(node.childSnapshotList, name)
+
+  def find_node(tree, name)
+    begin
+      snapshot = nil
+      tree.each do |node|
+        if node.name == name
+          snapshot = node.snapshot
+        elsif !node.childSnapshotList.empty?
+          snapshot = find_node(node.childSnapshotList, name)
+        end
       end
+      return snapshot
+    rescue Exception => e
+      puts "Exception occured with following message:"
+      puts e.message
     end
-    return snapshot
   end
-  
+
   private
+
   def vm
     dc = vim.serviceInstance.find_datacenter(resource[:datacenter])
     @vmObj ||= dc.find_vm(resource[:name]) or abort "VM not found."
