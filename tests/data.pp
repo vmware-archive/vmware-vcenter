f65e68608908de62a44182c80a96cff48f32946a
pankaj agarwal1 <pankaj_agarwal1@dell.com>
2013-12-17 06:57:07 +0000

    Puppet-lint formatting

--- tests/data.pp
+++ tests/data.pp
@@ -6,46 +6,37 @@ $vcenter = {
   'options'  => { 'insecure' => true }
 }
 
-
 $goldVMName = {
-  'name'  => 'Win7GoldVM_WithTools',
+  'name'  => 'Ubuntu_withTools',
 }
 
 $newVM = {
-  'vmName'  => 'WinCloneGuestVM',
-  
-  'memoryMB' => '2048',
-  'dnsDomain' => 'asm.test',  
-  'numCPU' => 2,
-  'cluster' => '',
-  'host' => '172.16.100.56',
-  'datacenter' => 'DDCQA',
-  'ensure' => 'present',
-  
-  'guestCustomization' => 'true',
-  'guesthostname' => 'ClonedVM',
-  
-  #'guesttype' => 'windows',
-  
-  'linuxtimezone'  => 'EST',
-  
-  'windowstimezone'  => '035',
-  'guestwindowsdomain'  => '',
-  'guestwindowsdomainadministrator'  => '',
-  'guestwindowsdomainadminpassword'  => '',
-  'windowsadminpassword'  =>iforgot,
-  'productid'  => '',
-  #'diskformat'  => 'Thin',
-  
-  'windowsguestowner' => 'TestOwner',
-  'windowsguestorgnization' => 'TestOrg',
-  
-  #'customizationlicensedatamode' => 'perServer',
-  #'autologon' => 'perServer',
-  'autologoncount' => '',
-  
-  'ip1' => "172.21.95.80",
-  'subnet1' => '255.255.240.0',
-  'dnsserver1' => '172.21.88.100',
-  'gateway1' => '172.21.95.254',
+  'vmName'                         => 'UbuntuCloneGuestVM',
+  'memoryMB'                       => '2048',
+  'dnsDomain'                      => 'asm.test',
+  'numCPU'                         => 2,
+  'cluster'                        => '',
+  'host'                           => '172.16.100.56',
+  'datacenter'                     => 'DDCQA',
+  'ensure'                         => 'absent',
+  'guestCustomization'             => true,
+  'guesthostname'                  => 'ClonedVM',
+  'guesttype'                      => 'linux',
+  'linuxtimezone'                  => 'EST',
+  'windowstimezone'                => '035',
+  'guestwindowsdomain'             => '',
+  'guestwindowsdomainadministrator'=> '',
+  'guestwindowsdomainadminpassword'=> '',
+  'windowsadminpassword'           =>iforgot,
+  'productid'                      => '',
+  #'diskformat'                    => 'Thin',
+  'windowsguestowner'              => 'TestOwner',
+  'windowsguestorgnization'        => 'TestOrg',
+  #'customizationlicensedatamode'  => 'perServer',
+  #'autologon'                     => 'perServer',
+  'autologoncount'                 => '',
+  'ip1'                            => '172.21.95.80',
+  'subnet1'                        => '255.255.240.0',
+  'dnsserver1'                     => '172.21.88.100',
+  'gateway1'                       => '172.21.95.254',
 }
--- tests/vc_vm.pp
+++ tests/vc_vm.pp
@@ -9,62 +9,57 @@ transport { 'vcenter':
 }
 
 vc_vm { $newVM['vmName']:
-  ensure     => $newVM['ensure'],
-  datacenter => $newVM['datacenter'],
-  goldvm => $goldVMName['name'],  
-  memorymb => $newVM['memoryMB'],
-  dnsdomain => $newVM['dnsDomain'],  
-  numcpu => $newVM['numCPU'],
-  transport  => Transport['vcenter'],
-  host => $newVM['host'],  
-  cluster => $newVM['cluster'],
-  diskformat  => $newVM['diskformat'],
-  
-  #Guest Customization Params
-  guestcustomization => $newVM['guestCustomization'],
-  guesthostname => $newVM['guesthostname'],  
-  guesttype => $newVM['guesttype'],  
-  
-  #Linux guest os specific
-  linuxtimezone => $newVM['linuxtimezone'],  
-  
-  #Windows guest os specific
-  windowstimezone  => $newVM['windowstimezone'],
-  guestwindowsdomain => $newVM['guestwindowsdomain'],
-  guestwindowsdomainadministrator => $newVM['guestwindowsdomainadministrator'],
-  guestwindowsdomainadminpassword => $newVM['guestwindowsdomainadminpassword'],
-  windowsadminpassword => $newVM['windowsadminpassword'],
-  productid  => $newVM['productid'],
-  
-  windowsguestowner  => $newVM['windowsguestowner'],
-  windowsguestorgnization  => $newVM['windowsguestorgnization'],
-  
-  customizationlicensedatamode => $newVM['customizationlicensedatamode'],
-  autologon => $newVM['autologon'],
-  autologoncount => $newVM['autologoncount'],
+    ensure                         => $newVM['ensure'],
+    datacenter                     => $newVM['datacenter'],
+    goldvm                         => $goldVMName['name'],
+    memorymb                       => $newVM['memoryMB'],
+    dnsdomain                      => $newVM['dnsDomain'],
+    numcpu                         => $newVM['numCPU'],
+    transport                      => Transport['vcenter'],
+    host                           => $newVM['host'],
+    cluster                        => $newVM['cluster'],
+    diskformat                     => $newVM['diskformat'],
+    #Guest Customization Params
+    guestcustomization             => $newVM['guestCustomization'],
+    guesthostname                  => $newVM['guesthostname'],
+    guesttype                      => $newVM['guesttype'],
+    #Linux guest os specific
+    linuxtimezone                  => $newVM['linuxtimezone'],
+    #Windows guest os specific
+    windowstimezone                => $newVM['windowstimezone'],
+    guestwindowsdomain             => $newVM['guestwindowsdomain'],
+  guestwindowsdomainadministrator  => $newVM['guestwindowsdomainadministrator'],
+  guestwindowsdomainadminpassword  => $newVM['guestwindowsdomainadminpassword'],
+  windowsadminpassword             => $newVM['windowsadminpassword'],
+  productid                        => $newVM['productid'],
+  windowsguestowner                => $newVM['windowsguestowner'],
+  windowsguestorgnization          => $newVM['windowsguestorgnization'],
+  customizationlicensedatamode     => $newVM['customizationlicensedatamode'],
+                         autologon => $newVM['autologon'],
+                    autologoncount => $newVM['autologoncount'],
 
-  #Guest OS nic specific params  
+  #Guest OS nic specific params
   nicspec => {
     nic => [{
-      ip    => $newVM['ip1'],
-      subnet => $newVM['subnet1'],
-	  dnsserver => $newVM['dnsserver1'],
-	  gateway => $newVM['gateway1']
+      ip        => $newVM['ip1'],
+      subnet    => $newVM['subnet1'],
+      dnsserver => $newVM['dnsserver1'],
+      gateway   => $newVM['gateway1']
     },{
-      ip    => "172.21.95.81",
-      subnet => $newVM['subnet1'],
-	  dnsserver => $newVM['dnsserver1'],
-	  gateway => $newVM['gateway1']
+      ip        => '172.21.95.81',
+      subnet    => $newVM['subnet1'],
+      dnsserver => $newVM['dnsserver1'],
+      gateway   => $newVM['gateway1']
     },{
-      ip    => "172.21.95.82",
-      subnet => $newVM['subnet1'],
-	  dnsserver => $newVM['dnsserver1'],
-	  gateway => $newVM['gateway1']
+      ip        => '172.21.95.82',
+      subnet    => $newVM['subnet1'],
+      dnsserver => $newVM['dnsserver1'],
+      gateway   => $newVM['gateway1']
     },{
-      ip    => "172.21.95.83",
-      subnet => $newVM['subnet1'],
-	  dnsserver => $newVM['dnsserver1'],
-	  gateway => $newVM['gateway1']
+      ip        => '172.21.95.83',
+      subnet    => $newVM['subnet1'],
+      dnsserver => $newVM['dnsserver1'],
+      gateway   => $newVM['gateway1']
     }],
   }
 }
