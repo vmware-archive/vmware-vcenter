5155d9db03a50876da4e025f76ac37027eec0082
neelam moorjani <neelam_moorjani@dell.com>
2013-12-24 09:21:42 +0000

    esx_portgroup.pp

---
+++ tests/esx_portgroup.pp
@@ -0,0 +1,29 @@
+# Copyright (C) 2013 VMware, Inc.
+import 'data.pp'
+
+transport { 'vcenter':
+  username => $vcenter['username'],
+  password => $vcenter['password'],
+  server   => $vcenter['server'],
+  options  => $vcenter['options'],
+}
+
+# This resource is not ready for testing:
+esx_portgroup { 'name':
+  name => "test25",
+  ensure => present,
+  type => "VMkernel",
+  vmotion => "Disabled",
+  ipsettings => "manual",
+  ipaddress => "172.16.12.16",
+  subnetmask => "255.255.0.0",
+  traffic_shaping_policy => "Disabled",
+  averagebandwidth => 1000,
+  peakbandwidth => 1000,
+  burstsize => 1024,
+  vswitch => vSwitch1,
+  host => "172.16.100.56",
+  path => "/Datacenter/cluster-1/",
+  vlanid => 5,
+  transport => Transport['vcenter'],
+}
