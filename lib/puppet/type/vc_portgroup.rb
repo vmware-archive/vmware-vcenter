156dc2b7a25e5e4aaecdfbf607001614860bc992
prakash kumar3 <prakash_kumar3@dell.com>
2013-12-24 06:48:18 +0000

    Added parameter validations in vc_portgroup.rb

--- lib/puppet/type/vc_portgroup.rb
+++ lib/puppet/type/vc_portgroup.rb
@@ -12,9 +12,8 @@ Puppet::Type.newtype(:vc_portgroup) do
     defaultto(:present)
   end
 
-
   newparam(:name, :namevar => true) do
-    desc "Virtual Switch port group name."
+    desc "vSwitch port group name."
     validate do |value|
       if value.strip.length == 0
         raise ArgumentError, "Invalid port group name."
@@ -24,7 +23,7 @@ Puppet::Type.newtype(:vc_portgroup) do
   end
 
   newproperty(:traffic_shaping_policy) do
-    desc "Traffic Shaping policy of the vm."
+    desc "Enable or Disable the traffic shaping policy on the vSwitch portgroup."
     newvalues(:Enabled, :Disabled)
   end
 
@@ -42,77 +41,76 @@ Puppet::Type.newtype(:vc_portgroup) do
     desc "Name of the vSwitch."
     validate do |value|
       if value.strip.length == 0
-        raise ArgumentError, "Invalid vswitch name."
+        raise ArgumentError, "Invalid vSwitch name."
       end
     end
   end
 
   newparam(:type) do
-  desc "type of port group"
-  dvalue="VirtualMachine"
-  defaultto(dvalue)
+    desc "Type of port group."
+    newvalues(:VirtualMachine, :VMkernel)
+    dvalue="VirtualMachine"
+    defaultto(dvalue)
   end
 
   newparam (:averagebandwidth) do
-  dvalue = 1000
-  defaultto(dvalue)
+    desc "Average bandwidth."
+    dvalue = 1000
+    defaultto(dvalue)
   end
 
   newparam (:peakbandwidth) do
-  dvalue = 1000
-  defaultto(dvalue)
+    desc "Peak bandwidth."
+    dvalue = 1000
+    defaultto(dvalue)
   end
 
   newparam (:burstsize) do
-  dvalue = 1024
-  defaultto(dvalue)
+    desc "Burst size."
+    dvalue = 1024
+    defaultto(dvalue)
   end
 
   newproperty (:vmotion) do
-  newvalues(:true, :false)
+    desc "Enable or Disable the vmotion on the VMkernel portgroup."
+    newvalues(:true, :false)
   end
   
   newproperty (:ipconfig) do
-  newvalues(:automatic, :manual)
+    desc "IP settings on the VMkernel port group."
+    newvalues(:automatic, :manual)
   end
 
   newparam(:ipaddress) do
-  desc "ipAddress of kernel port group"  
-  #validate do |value|
-  #if value.strip.length == 0
-  #	raise ArgumentError, "Invalid ipaddress of portgroup."
-  #end
-  #end
+    desc "IP address of VMkernel port group."
   end
 
   newparam (:subnetmask) do
-  desc "subnet mask of kernel port group"
-  #validate do |value|
-  #if value.strip.length == 0
-  #	raise ArgumentError, "Invalid subnet mask of portgroup."
-  #end
-  #end
+    desc "Subnet mask of VMkernel port group."
   end
 
   newparam(:path) do
     desc "The path to the host."
-
     validate do |path|
-      raise "Absolute path required: #{value}" unless Puppet::Util.absolute_path?(path)
+      raise ArgumentError, "Absolute path required: #{value}" unless Puppet::Util.absolute_path?(path)
     end
   end
 
- newparam(:host) do
+  newparam(:host) do
     desc "The Host IP."
- end
-
-  newproperty(:vlanid) do
-    desc "The numeric ID for the VLAN."
-	validate do |vlanid|
-		raise "vlanid must be in between 0 and 4095" if (vlanid.to_i<0 || vlanid.to_i > 4095)
-	end
-        defaultto 0
+    validate do |value|
+      if value.strip.length == 0
+        raise ArgumentError, "Invalid host name."
+      end
+    end
   end
 
+  newproperty(:vlanid) do
+    desc "VLAN id."
+	validate do |vlanid|
+	  raise ArgumentError, "VLAN id must be in between 0 and 4095." if (vlanid.to_i<0 || vlanid.to_i > 4095)
+	end
+    defaultto 0
+  end
 
 end
