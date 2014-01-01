32fa1e99ed1ed5623d43fdaafd8e9292a4be4355
Ashish Gupta14 <Ashish_Gupta14@dell.com>
2014-01-01 07:27:18 +0000

    Adding the esx_fc_mutilple_path_config_readme.md and updating the provider class message after infodev review Reviewed By:Vasanth

--- lib/puppet/provider/esx_fc_mutilple_path_config/default.rb
+++ lib/puppet/provider/esx_fc_mutilple_path_config/default.rb
@@ -8,12 +8,12 @@ Puppet::Type.type(:esx_fc_mutilple_path_config).provide(:esx_fc_mutilple_path_co
   def create
    begin 
      if host == nil
-       raise Puppet::Error, "Unable to find the host because host does not exists."
+       raise Puppet::Error, "Unable to find the host. The specified host does not exist."
      else	
        change_policy
      end   
     rescue Exception => ex
-        Puppet.err "Unable to perform the operation because the following exception occurred." 
+        Puppet.err "Unable to perform the operation because an unknown exception occurred. Verify the troubleshooting logs. If the issue persists, contact your service provider." 
         Puppet.err ex.message
    end    
   end
@@ -23,11 +23,11 @@ Puppet::Type.type(:esx_fc_mutilple_path_config).provide(:esx_fc_mutilple_path_co
 		isPolicyApplied = false
 		device_arr = host.configManager.storageSystem.storageDeviceInfo.multipathInfo.lun
 		if device_arr == nil
-			raise Puppet::Error, "Unable to find any native mutilpath storage devices on the given host #{host}."
+			raise Puppet::Error, "Unable to find any native multipath storage resources on the specified host #{host}."
 		else
 			device_arr.each do |change_policy|
 				if change_policy.path.length > 1 
-					Puppet.notice "Changing the multipath policy to #{resource[:policyname]} of FC/FCoE deviceID #{change_policy.id}"					
+					Puppet.notice "Changing the multipath policy to #{resource[:policyname]} for FC or FCoE deviceID #{change_policy.id} is in progress."					
 					policySpec = RbVmomi::VIM::HostMultipathInfoFixedLogicalUnitPolicy(:policy => resource[:policyname], :prefer => "*")
 					host.configManager.storageSystem.SetMultipathLunPolicy(:lunId => change_policy.id , :policy => policySpec)
 					isPolicyApplied = true			
@@ -35,11 +35,11 @@ Puppet::Type.type(:esx_fc_mutilple_path_config).provide(:esx_fc_mutilple_path_co
 			end
 		end
 		if isPolicyApplied == false
-			raise Puppet::Error, "Unable to find any FC/FCoe device for multipath policy changes."
+			raise Puppet::Error, "Unable to find any FC or FCoE resources for multipath policy changes."
 		end
 	end
     rescue Exception => ex
-        Puppet.err "Unable to change the policy to #{resource[:policyname]} because the following exception occurred."
+        Puppet.err "Unable to change the policy to #{resource[:policyname]} because an unknown exception occurred. Verify the troubleshooting logs. If the issue persists, contact your service provider."
         Puppet.err ex.message
 	end
 
