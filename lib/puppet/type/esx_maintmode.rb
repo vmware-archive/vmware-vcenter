408fcad409263f5f914422af8c39ddb3478c5c77
aditya shukla1 <aditya_shukla1@dell.com>
2014-01-08 11:45:02 +0000

    Incorporated code review comments by Dan Bode -proper commenting -backward compatibility

--- lib/puppet/type/esx_maintmode.rb
+++ lib/puppet/type/esx_maintmode.rb
@@ -2,51 +2,56 @@
 Puppet::Type.newtype(:esx_maintmode) do
   @doc = "Manage vsphere hosts entering / exiting maintenance mode."
 
-  ensurable do    
-newvalue(:present) do     
-        provider.create
-    end    
-newvalue(:absent) do     
-        provider.destroy
+  ensurable do
+    newvalue(:present) do
+      provider.create
     end
-    defaultto(:absent)
+    newvalue(:absent) do
+      provider.destroy
+    end
   end
 
- 
-  newparam(:host, :namevar => true) do
-    desc " Host DNS Name."
-  end
-    
-   
-  newparam(:timeout) do
-      desc "Timeout on maintenance mode operations.  Defaults to 0 (no timeout)."
-      newvalues(/\d+/)
-      defaultto(0)  
-      munge do |value|
-        Integer(value)
-      end
+  newparam(:hostseq, :namevar => true) do
+    desc "ESX hostname and optional sequence number.  format: host:seq"
+
+    munge do |value|
+      @resource[:host] = value.split(':',2)[0]
+      value
     end
-  
-    newparam(:evacuate_powered_off_vms) do
+  end
+
+  newparam(:host) do
+    desc "ESX hostname"
+  end
+
+  newparam(:timeout) do
+    desc "Timeout on maintenance mode operations.  Defaults to 0 (no timeout)."
+    newvalues(/\d+/)
+    defaultto(0)
+
+    munge do |value|
+      Integer(value)
+    end
+  end
+
+  newparam(:evacuate_powered_off_vms) do
     desc "Only supported by vcenter.  "\
          "If true, this will use DRS to migrate off powered down VMs before"\
          " completing the operation"
-      newvalues(:true, :false)
-      defaultto(:false)
-    # 
+    newvalues(:true, :false)
+    defaultto(:false)
+    #
     # The provider must accept and return Symbols :true and
     # :false, not TrueClass nor FalseClass. Methods is_to_s and
     # should_to_s clarify messages like 'changed true to true'
     # that would result from provider bugs.
-    # 
+    #
     def is_to_s(v)
       v.inspect
     end
+
     def should_to_s(v)
       v.inspect
     end
-      end
-  
-  
-  
+  end
 end
