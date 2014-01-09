3bde0183747a176e2e2e51d9d1b3576e46cc3a6f
pankaj agarwal1 <pankaj_agarwal1@dell.com>
2014-01-09 12:03:00 +0000

    Formatting changes

--- tests/data_registervm.pp
+++ tests/data_registervm.pp
@@ -3,15 +3,16 @@ $vcenter = {
   'username' => 'administrator',
   'password' => 'iforgot@123',
   'server'   => '172.16.100.167',
-  'options'  => { 'insecure' => true }
+  'options'  => {
+    'insecure' => true
+  }
 }
-
 
 $newVM = {
   'ensure'             => 'absent',
-  'name'               => 'test_pankaj',  
+  'name'               => 'test_pankaj',
   'hostip'             => '172.16.100.56',
-  'datacenter'         => 'DDCQA',  
+  'datacenter'         => 'DDCQA',
   'vmpath_ondatastore' => '[gale-fsr] testvm_pankaj/testvm_pankaj.vmx',
   'astemplate'         => 'false',
 }
--- tests/vc_vm_register.pp
+++ tests/vc_vm_register.pp
@@ -9,12 +9,11 @@ transport { 'vcenter':
 }
 
 vc_vm_register { $newVM['name']:
-  ensure              => $newVM['ensure'],
-  transport           => Transport['vcenter'],
-  datacenter          => $newVM['datacenter'],  
-  hostip              => $newVM['hostip'],
-  astemplate          => $newVM['astemplate'],
-  vmpath_ondatastore  => $newVM['vmpath_ondatastore'],  
- }
-
+  ensure             => $newVM['ensure'],
+  transport          => Transport['vcenter'],
+  datacenter         => $newVM['datacenter'],
+  hostip             => $newVM['hostip'],
+  astemplate         => $newVM['astemplate'],
+  vmpath_ondatastore => $newVM['vmpath_ondatastore'],
+}
 
