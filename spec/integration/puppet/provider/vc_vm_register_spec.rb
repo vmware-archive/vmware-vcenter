12129db7a4e0eb2cdd62afb08cefef3eb99410c7
pawan gautam <pawan_gautam@dell.com>
2014-01-13 08:28:08 +0000

    Commiting Integration rspec changes.

---
+++ spec/integration/puppet/provider/vc_vm_register_spec.rb
@@ -0,0 +1,73 @@
+#! /usr/bin/env ruby
+
+require 'spec_helper'
+require 'yaml'
+require 'puppet/provider/vcenter'
+require 'rbvmomi'
+
+describe Puppet::Type.type(:vc_vm_register).provider(:vc_vm_register) do  
+  vc_vm_register_yml =  YAML.load_file(my_fixture('vc_vm_register.yml'))
+  registervm = vc_vm_register_yml['registervm']  
+  
+  transport_yml =  YAML.load_file(my_fixture('transport.yml'))
+  transport_node = transport_yml['transport']  
+
+  let(:register_vm) do
+    @catalog = Puppet::Resource::Catalog.new
+      transport = Puppet::Type.type(:transport).new({
+      :name => transport_node['name'],
+      :username => transport_node['username'],
+      :password => transport_node['password'],
+      :server   => transport_node['server'],
+      :options => transport_node['options'],
+     })
+    @catalog.add_resource(transport)
+
+	Puppet::Type.type(:vc_vm_register).new(
+	    :name               => registervm['name'],
+		:ensure             => registervm['ensure'],
+		:transport          => transport,	
+		:catalog            => @catalog,
+		:hostip             => registervm['hostip'],
+		:datacenter         => registervm['datacenter'],  
+		:vmpath_ondatastore => registervm['vmpath_ondatastore'],
+		:astemplate         => registervm['astemplate']
+    )
+  end  
+
+  removevm = vc_vm_register_yml['removevm']
+  
+  let :remove_vm do
+    @catalog = Puppet::Resource::Catalog.new
+      transport = Puppet::Type.type(:transport).new({
+      :name => transport_node['name'],
+      :username => transport_node['username'],
+      :password => transport_node['password'],
+      :server   => transport_node['server'],
+      :options  => transport_node['options'],
+     })
+
+    @catalog.add_resource(transport)
+
+	Puppet::Type.type(:vc_vm_register).new(
+	    :name               => removevm['name'],
+		:ensure             => removevm['ensure'],
+		:transport          => transport,	
+		:catalog            => @catalog,
+		:hostip             => removevm['hostip'],
+		:datacenter         => removevm['datacenter']
+    )
+  end
+
+  describe "when registering a vm" do     
+    it "should be able to register vm" do
+      register_vm.provider.create
+    end
+  end
+
+  describe "when removing a vm - which already exists" do
+    it "should be able to remove vm" do
+      remove_vm.provider.destroy
+    end
+  end
+end
