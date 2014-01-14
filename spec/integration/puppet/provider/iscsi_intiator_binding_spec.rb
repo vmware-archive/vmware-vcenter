55e2a7a35ef158eb1eb58a45d245e1e4d8a4fed8
ritu gupta3 <ritu_gupta3@dell.com>
2014-01-14 11:21:31 +0000

    RSpec for vc_migratevm functionality has been added

--- spec/integration/puppet/provider/iscsi_intiator_binding_spec.rb
+++ spec/integration/puppet/provider/iscsi_intiator_binding_spec.rb
@@ -8,16 +8,12 @@ require 'rbvmomi'
 describe Puppet::Type.type(:iscsi_intiator_binding).provider(:iscsi_intiator_binding) do
 
   iscsi_intiator_binding_yml =  YAML.load_file(my_fixture('iscsi_intiator_binding.yml'))
-   initiator_binding = iscsi_intiator_binding_yml['initiator_binding']
+  initiator_binding = iscsi_intiator_binding_yml['initiator_binding']
 
   transport_yml =  YAML.load_file(my_fixture('transport.yml'))
   transport_node = transport_yml['transport']
-    
-    
 
   let(:bind_iscsi_initiator) do
-    puts "calling"
-    puts transport_node.inspect
     @catalog = Puppet::Resource::Catalog.new
     transport = Puppet::Type.type(:transport).new({
       :name => transport_node['name'],
@@ -40,24 +36,57 @@ describe Puppet::Type.type(:iscsi_intiator_binding).provider(:iscsi_intiator_bin
     )
   end
 
+  initiator_unbinding = iscsi_intiator_binding_yml['initiator_unbinding']
+
+  let(:unbind_iscsi_initiator) do
+    @catalog = Puppet::Resource::Catalog.new
+    transport = Puppet::Type.type(:transport).new({
+      :name => transport_node['name'],
+      :username => transport_node['username'],
+      :password => transport_node['password'],
+      :server   => transport_node['server'],
+      :options  => transport_node['options'],
+    })
+    @catalog.add_resource(transport)
+
+    Puppet::Type.type(:iscsi_intiator_binding).new(
+    :name                   => initiator_unbinding['name'],
+    :ensure                 => initiator_unbinding['ensure'],
+    :transport              => transport,
+    :catalog                => @catalog,
+    :vmknics                => initiator_unbinding['vmknics'],
+    :script_executable_path => initiator_unbinding['script_executable_path'],
+    :host_username          => initiator_unbinding['host_username'],
+    :host_password          => initiator_unbinding['host_password']
+    )
+  end
 
   let(:provider) do
     described_class.new( )
   end
 
   describe "when binding a iscsi initiator" do
-    it "should be able bind iscsi initiator" do
+    it ", iscsi binding should exist before initiating destroy operation" do
+      response = bind_iscsi_initiator.provider.is_binded
+      response.should be_false
+    end
+
+    it ", should be able bind iscsi initiator" do
       bind_iscsi_initiator.provider.create
       response = bind_iscsi_initiator.provider.is_binded
       response.should be_true
-      end
+    end
   end
 
   describe "when undinding a iscsi initiator - which already bind" do
-    it "should be able to unbind iscsi initiator " do
-      puts "inside destroy"
-      bind_iscsi_initiator.provider.destroy
+    it ", iscsi binding should exist before initiating destroy operation" do
       response = bind_iscsi_initiator.provider.is_binded
+      response.should be_true
+    end
+    it ", should be able to unbind iscsi initiator " do
+
+      unbind_iscsi_initiator.provider.destroy
+      response = unbind_iscsi_initiator.provider.is_binded
       response.should be_false
     end
   end
