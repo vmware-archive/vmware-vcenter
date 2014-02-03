ed9a116d87735844d469e9ba401c82173210a76e
Dan <bodepd@gmail.com>
2014-02-03 14:27:04 -0800

    Update esx_datastore example
    TO show how pun or target_iqn can be used.

--- tests/esx_datastore.pp
+++ tests/esx_datastore.pp
@@ -8,10 +8,13 @@ transport { 'vcenter':
   options  => $vcenter['options'],
 }
 
-esx_datastore { "${esx1['hostname']}:nfs_store":
-  ensure      => present,
-  type        => 'nfs',
-  remote_host => '192.168.232.1',
-  remote_path => '/Users/nan/src/nodejs',
-  transport   => Transport['vcenter'],
+esx_datastore { "${esx1['hostname']}:<vmfs_datastore>":
+  ensure       => present,
+  type         => 'vmfs',
+  target_iqn   => 'fc.5000d310005ec401:5000d310005ec437', # in case of iSCSI, FC or FCoE luns
+  #path         => '/AS1000DC/DDCCluster/',
+  #lun         => '100',                                 # in case of SCSI lun
+  #remote_host => '192.168.232.1',                       # in case of nfs datastore
+  #remote_path => '/Users/nan/src/nodejs',               # in case of nfs datastore
+  transport    => Transport['vcenter'],
 }
