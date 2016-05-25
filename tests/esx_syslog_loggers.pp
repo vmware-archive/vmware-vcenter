11a2a04949d285fd86be9fea4098197e1ca3b6b3
Mohan Balasundaram <mbalasundaram@skyscapecloud.com>
2016-05-25 19:28:40 +0100

    updated the reference values to strings to maintain esx_advanced_opts idempotency

--- tests/esx_syslog_loggers.pp
+++ tests/esx_syslog_loggers.pp
@@ -21,14 +21,14 @@ vcenter::syslog_loggers { $esx1['hostname']:
   esxi_version                              => 6,         #version can be 5 or 6
   transport_string                          => 'vcenter', #use the same transport string
   logger_options                            => {
-    "Syslog.loggers.hostd.rotate"           => 20,
-    "Syslog.loggers.hostd.size"             => 2048,
-    "Syslog.loggers.vmkernel.rotate"        => 20,
-    "Syslog.loggers.vmkernel.size"          => 2048,
-    "Syslog.loggers.fdm.rotate"             => 20,
-    "Syslog.loggers.fdm.size"               => 2048,
-    "Syslog.loggers.vpxa.rotate"            => 20,
-    "Syslog.loggers.vpxa.size"              => 2048,
+    "Syslog.loggers.hostd.rotate"           => "20",
+    "Syslog.loggers.hostd.size"             => "2048",
+    "Syslog.loggers.vmkernel.rotate"        => "20",
+    "Syslog.loggers.vmkernel.size"          => "2048",
+    "Syslog.loggers.fdm.rotate"             => "20",
+    "Syslog.loggers.fdm.size"               => "2048",
+    "Syslog.loggers.vpxa.rotate"            => "20",
+    "Syslog.loggers.vpxa.size"              => "2048",
   }
 }
 
