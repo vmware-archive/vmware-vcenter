bf3b83293391c7b954bd39ab35a940ffa12fb0f2
Sushil_R <Sushil_R@Dell.com>
2016-08-23 19:00:57 +0530

    ASM-7640 Warning Messages thrown in FC 630 cluster for VSAN
    Added support for enabling / disabling VSAN health performance services. By default health services is not enabled when VSAN is enabled on the cluster and due to this, a warning message is shown in VSAN health table

---
+++ lib/puppet/provider/vc_vsan_health_performance/default.rb
@@ -0,0 +1,44 @@
+provider_path = Pathname.new(__FILE__).parent.parent
+require File.join(provider_path, 'vcenter')
+require 'rbvmomi'
+require File.join(provider_path, 'vsanmgmt.api')
+require File.join(provider_path, 'vsanapiutils')
+
+Puppet::Type.type(:vc_vsan_health_performance).provide(:vc_vsan_health_performance, :parent => Puppet::Provider::Vcenter) do
+  @doc = "Enable / Disable VSAN property."
+
+  def create
+    Puppet.debug("Configuring VSAN performance service")
+    vsan.vsanPerformanceManager.VsanPerfCreateStatsObjectTask(:cluster => cluster).onConnection(vim).wait_for_completion
+  end
+
+  def destroy
+    Puppet.debug("De-Configuring VSAN performance service")
+    vsan.vsanPerformanceManager.VsanPerfDeleteStatsObject(:cluster => cluster)
+  end
+
+  def exists?
+    status = JSON.parse(vsan.vsanClusterHealthSystem.VsanHealthGetClusterStatus(:cluster => cluster))
+    Puppet.debug("VSAN Health Servce Status: #{status}")
+
+    group_health = vsan.vsanPerformanceManager.VsanPerfQueryClusterHealth(:cluster => cluster)[0].groupHealth
+    if resource[:ensure] == :present
+      group_health != "green" ? false : true
+    elsif resource[:ensure] == :absent
+      group_health != "green" ? false : true
+    end
+  end
+
+  def datacenter
+    @dc ||= vim.serviceInstance.find_datacenter(resource[:datacenter])
+  end
+
+  def cluster
+    datacenter.find_compute_resource(resource[:cluster])
+  end
+
+  def vsan
+    @vsan ||= vim.vsan
+  end
+
+end
