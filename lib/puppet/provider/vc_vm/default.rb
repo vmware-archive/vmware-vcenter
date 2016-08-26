29010ae62e4aa609c5dd6aea7e23dc53e1be169d
Sushil_R <Sushil_R@Dell.com>
2016-08-26 16:20:45 +0530

    ASM-7729 VM clone operation fails when performed across cluster or datacenter with no common datastore
    For clone operation datatore name is not passed for relocation spec passed to VM clone operation. These changes seems to be got broken due some recent changes while adding virtual disk support.

--- lib/puppet/provider/vc_vm/default.rb
+++ lib/puppet/provider/vc_vm/default.rb
@@ -771,7 +771,8 @@ Puppet::Type.type(:vc_vm).provide(:vc_vm, :parent => Puppet::Provider::Vcenter) 
   end
 
   def datastore_object(datastore_name)
-    cluster.datastore.find { |ds| ds_obj = ds if "[#{ds.name}]" == datastore_name}
+    datastore_name = datastore_name.is_a?(Hash) ? datastore_name["name"] : datastore_name
+    cluster.datastore.find { |ds| ds_obj = ds if "[#{ds.name}]" == datastore_name || ds.name == datastore_name}
   end
 
   private
